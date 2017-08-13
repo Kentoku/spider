f0438bfaa6c81257e8d95a9b1cc993a5f5b44333
Kentoku <kentokushiba@gmail.com>
2017-08-13 20:13:08 +0900

    add case of HA_EXTRA_USE_CMP_REF in extra().

--- ha_spider.cc
+++ ha_spider.cc
@@ -1934,8 +1934,13 @@ int ha_spider::extra(
         DBUG_RETURN(error_num);
       break;
 #endif
+#if defined(HA_EXTRA_HAS_STARTING_ORDERED_INDEX_SCAN) || defined(HA_EXTRA_HAS_HA_EXTRA_USE_CMP_REF)
 #ifdef HA_EXTRA_HAS_STARTING_ORDERED_INDEX_SCAN
     case HA_EXTRA_STARTING_ORDERED_INDEX_SCAN:
+#endif
+#ifdef HA_EXTRA_HAS_HA_EXTRA_USE_CMP_REF
+    case HA_EXTRA_USE_CMP_REF:
+#endif
       DBUG_PRINT("info",("spider HA_EXTRA_STARTING_ORDERED_INDEX_SCAN"));
       if (table_share->primary_key != MAX_KEY)
       {
