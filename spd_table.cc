39057a3ec4f475f7428989d7432376fdfa8fbf90
Kentoku <kentokushiba@gmail.com>
2017-10-30 00:49:54 +0900

    remove needless check

--- spd_table.cc
+++ spd_table.cc
@@ -570,15 +570,12 @@ int spider_free_share_alloc(
 ) {
   int roop_count;
   DBUG_ENTER("spider_free_share_alloc");
-  if (share->dbton_bitmap)
+  for (roop_count = SPIDER_DBTON_SIZE - 1; roop_count >= 0; roop_count--)
   {
-    for (roop_count = SPIDER_DBTON_SIZE - 1; roop_count >= 0; roop_count--)
+    if (share->dbton_share[roop_count])
     {
-      if (share->dbton_share[roop_count])
-      {
-        delete share->dbton_share[roop_count];
-        share->dbton_share[roop_count] = NULL;
-      }
+      delete share->dbton_share[roop_count];
+      share->dbton_share[roop_count] = NULL;
     }
   }
   if (share->server_names)
